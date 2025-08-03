02af4f66bc681f8fd143f5c41fb5f9b3d44ba00d
harshjha047 <spyderhj11@gmail.com>
2025-08-03 23:35:22 +0530

    fonts

--- src.js
+++ src.js
@@ -58,51 +58,61 @@ let page5 = () => {
   const data = [
     {
       sname: "shristi",
+      instaID:"https://www.instagram.com/_.shrxxti._?igsh=dzlxc3F3ZTVxN2Rw",
       position: " President",
       img: "./public/shristi.jpg",
     },
     {
       sname: "anushka jha",
+      instaID:"https://www.instagram.com/anushka_jha._?igsh=MW4zczZha2Nkcno5",
       position: "vice President",
       img: "public/anushka.jpg",
     },
     {
       sname: "yashraj",
+      instaID:"https://www.instagram.com/yash_raj0813?igsh=NGF6YWQ0NDNneGN6",
       position: "assistant Secretary",
       img: "public/yashraj.jpg",
     },
     {
       sname: "priyani",
+      instaID:"https://www.instagram.com/_.priyaniii._?igsh=MWgya2Vmd3M5bHRncg==",
       position: "Theater",
       img: "public/priyani.jpg",
     },
     {
       sname: "avinash",
+      instaID:"https://www.instagram.com/avinasshh_ig?igsh=cGd4MmlpZ2l3b2lp",
       position: "Music",
       img: "public/avi.jpg",
     },
     {
       sname: "aryan",
+      instaID:"https://www.instagram.com/_aka.aryan_?igsh=YzR6cHplb25kdjUz",
       position: "Dance",
       img: "public/aryan.jpg",
     },
     {
       sname: "mahek Goinka",
+      instaID:"https://www.instagram.com/meeemahekkk?igsh=MXV1NWpveDd0ZXlxNA==",
       position: "Management",
       img: "public/mahek.jpg",
     },
     {
       sname: "nikhil singh bora",
+      instaID:"https://www.instagram.com/singh_niikhil?igsh=MXF3dmcxOWRtdHA4eg==",
       position: "Management",
       img: "public/nikhil.jpg",
     },
     {
       sname: "harshit",
+      instaID:"https://www.instagram.com/theharshitbhutani?igsh=NjVhdm84endpb21i",
       position: "social media",
       img: "public/harshit.jpg",
     },
     {
       sname: "khushi gupta",
+      instaID:"https://www.instagram.com/khushigg15?igsh=ZjRvMjVkYWJpZTRr",
       position: "creative",
       img: "public/khushi.jpg",
     },
@@ -112,9 +122,9 @@ let page5 = () => {
     div.setAttribute("class", "spic");
 
     div.innerHTML = `
-          <div class="spicName">${res.sname}</div>
+          <a href="${res.instaID}"><div class="spicName">${res.sname}</div>
           <div class="spicDis">${res.position}</div>
-          <img src="${res.img}" alt="" />
+          <img src="${res.img}" alt="" /></a>
     `;
     simg.appendChild(div);
   });

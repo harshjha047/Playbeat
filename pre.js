8220363fbac41ef196b8f47f675782fd6f90a221
harshjha047 <spyderhj11@gmail.com>
2025-08-04 14:22:48 +0530

    scalling

---
+++ pre.js
@@ -0,0 +1,52 @@
+window.addEventListener("load", () => {
+  const images = [
+    "public/anushka.jpg",
+    "public/yashraj.jpg",
+    "public/sikha.jpg",
+    "public/shristi.jpg",
+    "public/richa.jpeg",
+    "public/priyani.jpg",
+    "public/prinka.jpg",
+    "public/nikhil.jpg",
+    "public/mahek.jpg",
+    "public/log.jpeg",
+    "public/log-removebg-preview.png",
+    "public/khushi.jpg",
+    "public/IMG_0442.jpg",
+    "public/harshit.jpg",
+    "public/avi.jpg",
+    "public/aryan.jpg",
+  ];
+
+  const fonts = [
+    new FontFace('Salita', 'url(./salita/Salita.ttf)'),
+    new FontFace('Carlo', 'url(./carlo/Carlo.ttf)'),
+    new FontFace('visoria', 'url(./Visoria/visoria-regular.ttf)'),
+    new FontFace('WalkieValkyrie', 'url(./walkievalkyrie/WalkieValkyrie-Regular.ttf)'),
+  ];
+
+  const preloadImages = images.map(src => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.src = src;
+      img.onload = resolve;
+      img.onerror = reject;
+    });
+  });
+
+  const preloadFonts = fonts.map(font => {
+    return font.load().then(loadedFont => {
+      document.fonts.add(loadedFont);
+    });
+  });
+
+  Promise.all([...preloadImages, ...preloadFonts])
+    .then(() => {
+      // All done
+      document.getElementById('loader').style.display = 'none';
+      document.getElementById('mainContent').style.display = 'block';
+    })
+    .catch(err => {
+      console.error("Error loading assets:", err);
+    });
+});
